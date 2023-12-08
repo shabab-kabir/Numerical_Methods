49c22ebfc4bc78a7b3de2760d3957c4bec0d5258
Shabab M.K <kabirsha@grinnell.edu>
2023-12-07 22:30:22 -0600

    Update cumulativeDistanceLUT.m

--- cumulativeDistanceLUT.m
+++ cumulativeDistanceLUT.m
@@ -1,11 +1,18 @@
 function distanceLUT = cumulativeDistanceLUT(curvePoints)
     % Initialize the distanceLUT array
     numPoints = size(curvePoints, 1);
-    distanceLUT = zeros(numPoints, 1);
+    
+    % Initialize the LUT with two columns: first for t values, second for distances
+    distanceLUT = zeros(numPoints, 2);
+
+    % Set the initial t value (assuming it starts at 0)
+    distanceLUT(1, 1) = 0; % t value
+    distanceLUT(1, 2) = 0; % cumulative distance
 
     % Compute the cumulative distances
     for i = 2:numPoints
         segmentLength = norm(curvePoints(i, :) - curvePoints(i - 1, :));
-        distanceLUT(i) = distanceLUT(i - 1) + segmentLength;
+        distanceLUT(i, 1) = (i - 1) / (numPoints - 1); % Normalized t value
+        distanceLUT(i, 2) = distanceLUT(i - 1, 2) + segmentLength;
     end
 end
